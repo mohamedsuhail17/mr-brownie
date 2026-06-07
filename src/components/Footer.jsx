3ce5bae89d461efc2f68d4fc10afb411fcb708b1
Mohamed Suhail J <mdsuhail5055@gmail.com>
2026-06-07 18:12:24 +1100

    Added Phone Icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
+import { LuPhone } from "react-icons/lu"; 
 export default function Footer(){
     return (<footer className="bg-black text-white flex justify-center gap-8 text-sm">
         <span>&copy; {new Date().getFullYear()} M.R Homemade Brownies. All Rights reserved</span>
-        <span></span>
-        <span>Contact us<a href='tel:+916385292762'>+91 6385292762</a></span>
+        
+        <span ><a href='tel:+918270679793' className="flex items-center align gap-1"><LuPhone /> +91 82706 79793</a></span>
     </footer>)
 }
